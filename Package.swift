c606e9879fdfdc3bfa2f8a79b2e366593eb85137
Takashi Bando <takashi@m-intuition.com>
2024-03-15 11:35:25 -0700

    For release 0.0.5

--- Package.swift
+++ Package.swift
@@ -15,14 +15,8 @@ let package = Package(
     dependencies: [],
     targets: [
         .binaryTarget(
-            name: "whisper.cppBinding",
+            name: "whisper.cpp",
             url: "https://github.com/dstbando/whisper.cpp/releases/download/0.0.4/whisper.cpp.xcframework.zip",
             checksum: "ad6cf74577b6ee44a8c484ed202bdcf10531563c801b1750453a0920fe5c8308"),
-        .target(
-            name: "whisper.cpp",
-            dependencies: []),
-        .testTarget(
-            name: "whisper.cppTest",
-            dependencies: []),
     ]
 )
